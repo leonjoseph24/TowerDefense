6e16e8307ba4bd1a6cc56064afa0468635d3ec0d
Leon <p.fernandes@saedubai.com>
2017-04-04 16:42:50 +0400

    latest

---
+++ Assets/Scripts/EnemyDmg.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyDmg : MonoBehaviour {
+    public int HP;
+    public goldCS pl;
+    public ScoreCS sc;
+    // Use this for initialization
+    void Start () {
+       
+	}
+
+    // Update is called once per frame
+     void Awake()
+    {
+        pl = GameObject.Find("Player").GetComponent<goldCS>();
+        sc = GameObject.Find("Player").GetComponent<ScoreCS>();
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            Destroy(other.gameObject);
+            
+            HP -= 1;
+            
+        }
+        if (HP <= 0)
+        {
+            Destroy(this.gameObject);
+            
+            pl.gold +=40;
+            Debug.Log("Gold"+pl.gold);
+            sc.score += 1;
+            Debug.Log("Current Score" + sc.score);
+        }
+        if (sc.score >= 50)
+        {
+            SceneManager.LoadScene("Victory");
+        }
+
+        
+    }
+    
+}
---
+++ Assets/Scripts/goldCS.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class goldCS : MonoBehaviour {
+    public int gold;
+    public GameObject goldTextObj;
+    Text goldText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        goldText = goldTextObj.GetComponent<Text>();
+        goldText.text = "Gold:" + gold.ToString();
+	}
+
+
+}
